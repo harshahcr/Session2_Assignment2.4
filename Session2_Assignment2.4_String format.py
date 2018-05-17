8027d2f0196d963b7d4b19ed77ef750faea04f1e
harshahcr <39063531+harshahcr@users.noreply.github.com>
2018-05-17 15:56:37 +0530

    Add files via upload

---
+++ Session2_Assignment2.4_String format.py
@@ -0,0 +1,16 @@
+#Printing string in specified format using space count
+x = ' '
+line1 = str('WE, THE PEOPLE OF INDIA,')
+line2 = str('having solemnly resolved to constitute India into a SOVEREIGN,!')
+line3 = str('SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC')
+line4 = str('and to secure to all its citizens')
+print(line1, '\n', +4*x,line2, '\n',+11*x, line3, '\n',+12*x, line4)
+
+
+#Printing string in specified format using tab
+line1 = str('WE, THE PEOPLE OF INDIA,')
+line2 = str('having solemnly resolved to constitute India into a SOVEREIGN,!')
+line3 = str('SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC')
+line4 = str('and to secure to all its citizens')
+print(line1, '\n', '\t',line2, '\n','\t','\t', line3, '\n','\t','\t', line4)
+
